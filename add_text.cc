40ab38c287bd033a7e98ec790501d32e3e76cecb
nguyễn văn công <congnguyen@MacBook-Pro-cua-nguyen.local>
2024-07-25 15:54:52 +0700

    Fix issues

--- add_text.cc
+++ add_text.cc
@@ -36,42 +36,92 @@
 #include "tscore/ink_defs.h"
 
 #define PLUGIN_NAME "add_text"
+#define FOOTER_PRE "##Total lines:"
+static const char * filepath = nullptr;
 
 #define ASSERT_SUCCESS(_x) TSAssert((_x) == TS_SUCCESS)
 
-struct MyData {
-  TSVIO            output_vio;
-  TSIOBuffer       output_buffer;
+struct MyData
+{
+  TSVIO output_vio;
+  TSIOBuffer output_buffer;
   TSIOBufferReader output_reader;
-  bool             added_prepend;
-  int              line_count;
-  bool  header_added;
+  bool header_added;
   bool footer_added;
+  int line_count;
+  int header_length;
+  int footer_length;
+  char *header;
+  char *footer;
 };
 
-static TSIOBuffer       comment_buffer;
-static TSIOBufferReader comment_buffer_reader;
-static int              comment_buffer_length;
+static char *
+load_header_in_file(const char *filename, int &file_size)
+{
+  FILE *file;
+  char *buffer;
+  // Open the file in read mode
+  file = fopen(filename, "r");
+  if (file == NULL)
+  {
+    TSError("[%s]Error opening file\n[%s]", PLUGIN_NAME, filename);
+    return nullptr;
+  }
 
-static TSIOBuffer       footer_buffer;
-static TSIOBufferReader footer_buffer_reader;
+  // Get the size of the file
+  fseek(file, 0, SEEK_END); // Move to the end of the file
+  file_size = ftell(file);  // Get the current position (size of the file)
+  rewind(file);             // Move back to the beginning of the file
 
-static char *prepend_text        = nullptr; // Prepend text from the config
-static int   prepend_text_length = 0;
+  // Allocate memory for the buffer
+  buffer = (char *)malloc((file_size + 1) * sizeof(char));
+  if (buffer == NULL)
+  {
+    perror("Memory allocation failed");
+    fclose(file);
+    return nullptr;
+  }
+
+  // Read the file content into the buffer
+  fread(buffer, sizeof(char), file_size, file);
+  // Clean up
+  fclose(file);
+
+  return buffer;
+}
 
 static MyData *
-my_data_alloc()
+my_data_alloc(const char *filename)
 {
   MyData *data = static_cast<MyData *>(TSmalloc(sizeof(MyData)));
   TSReleaseAssert(data);
 
-  data->output_vio    = nullptr;
-  data->output_buffer = nullptr;
-  data->output_reader = nullptr;
-  data->added_prepend = false;
+  data->output_vio = nullptr;
+  data->output_buffer = TSIOBufferCreate();
+  data->output_reader = TSIOBufferReaderAlloc(data->output_buffer);
   data->header_added = false;
+  data->line_count = 0;
   data->footer_added = false;
-  data->line_count    = 0;
+
+  int file_size;
+  char *buffer = load_header_in_file(filename, file_size);
+  if (buffer == nullptr)
+  {
+    int n = strlen(filename);
+    int len = n + 5;
+    char header_tmp[len];
+    memset(header_tmp, 0, len);
+    snprintf(header_tmp, len, "##%s\n", filename);
+    data->header = strdup(header_tmp);
+  }
+  else
+  {
+    char header_tmp[file_size + 3];
+    memset(header_tmp, 0, file_size + 3);
+    snprintf(header_tmp, file_size + 3, "##%s\n", buffer);
+    data->header = strdup(header_tmp);
+  }
+  data->header_length = strlen(data->header);
 
   return data;
 }
@@ -79,12 +129,20 @@ my_data_alloc()
 static void
 my_data_destroy(MyData *data)
 {
-  if (prepend_text) {
-    free(prepend_text);
-    prepend_text = NULL;
+  printf("my_data_destroy\n");
+  if (data->header) {
+    free(data->header);
+    data->header = NULL;
+  } 
+  if (data->footer) {
+    free(data->footer);
+    data->footer = NULL;
   }
-  if (data) {
-    if (data->output_buffer) {
+
+  if (data)
+  {
+    if (data->output_buffer)
+    {
       TSIOBufferDestroy(data->output_buffer);
     }
     TSfree(data);
@@ -95,10 +153,9 @@ static void
 handle_transform(TSCont contp)
 {
   TSVConn output_conn;
-  TSVIO   write_vio;
-  MyData *data;
+  TSVIO write_vio;
   int64_t towrite;
-
+  MyData *data;
   /* Get the output connection where we'll write data to. */
   output_conn = TSTransformOutputVConnGet(contp);
 
@@ -114,39 +171,38 @@ handle_transform(TSCont contp)
      and initialize its internals. */
   data = static_cast<decltype(data)>(TSContDataGet(contp));
   if (!data) {
-    towrite             = TSVIONBytesGet(write_vio);
-    data                = my_data_alloc();
-    data->output_buffer = TSIOBufferCreate();
-    data->output_reader = TSIOBufferReaderAlloc(data->output_buffer);
-    // data->output_vio = TSVConnWrite(output_conn, contp, data->output_reader, towrite);
+    printf("my_data_alloc\n");
+    data                = my_data_alloc(filepath);
     TSContDataSet(contp, data);
   }
 
   towrite = TSVIONTodoGet(write_vio);
-  if (towrite > 0) {
+  if (towrite > 0)
+  {
     int64_t avail = TSIOBufferReaderAvail(TSVIOReaderGet(write_vio));
-    if (towrite > avail) {
+    if (towrite > avail)
+    {
       towrite = avail;
     }
 
-    if (towrite > 0) {
+    if (towrite > 0)
+    {
       // Count lines and copy the data
 
-      if (!data->header_added) {
-         prepend_text_length += 3;
-         char header[prepend_text_length];
-         header[prepend_text_length] = '\0';
-        snprintf(header, sizeof(header), "##%s\n", prepend_text);
-        TSIOBufferWrite(data->output_buffer, header, prepend_text_length);
+      if (!data->header_added)
+      {
+        TSIOBufferWrite(data->output_buffer, data->header, data->header_length);
         data->header_added = true;
       }
-
       TSIOBufferBlock blk = TSIOBufferReaderStart(TSVIOReaderGet(write_vio));
-      while (blk) {
-        int64_t     block_avail;
+      while (blk)
+      {
+        int64_t block_avail;
         const char *block_start = TSIOBufferBlockReadStart(blk, TSVIOReaderGet(write_vio), &block_avail);
-        for (int64_t i = 0; i < block_avail; i++) {
-          if (block_start[i] == '\n') {
+        for (int64_t i = 0; i < block_avail; i++)
+        {
+          if (block_start[i] == '\n')
+          {
             data->line_count++;
           }
         }
@@ -154,49 +210,64 @@ handle_transform(TSCont contp)
         TSIOBufferWrite(data->output_buffer, block_start, block_avail);
         blk = TSIOBufferBlockNext(blk);
       }
-    
+
       TSIOBufferReaderConsume(TSVIOReaderGet(write_vio), towrite);
       TSVIONDoneSet(write_vio, TSVIONDoneGet(write_vio) + towrite);
-     
-    }
-     
-      // if (data->output_vio) {
-      //    TSVIOReenable(data->output_vio);
-      // }
-      if (TSVIONTodoGet(write_vio) > 0) {
-           TSVIOReenable(write_vio);
-      } else {
-         if (!data->footer_added) {
-         data->footer_added = true;
-           char footer[256];
-          snprintf(footer, sizeof(footer), "##Totals line:%d", data->line_count);
-          TSIOBufferWrite(data->output_buffer, footer, strlen(footer));
-          data->output_vio = TSVConnWrite(output_conn, contp, data->output_reader, towrite + strlen(footer) + prepend_text_length);
-         }
+
+      if (TSVIONTodoGet(write_vio) > 0)
+      {
+        TSVIOReenable(write_vio);
       }
-     
-    // Send an event back to the upstream if required
-    if (TSVIONTodoGet(write_vio) == 0 && data->footer_added) {
-        TSContCall(TSVIOContGet(write_vio), TS_EVENT_VCONN_WRITE_COMPLETE, write_vio);
+      else
+      {
+        if (!data->footer_added)
+        {
+          data->footer_added = true;
+          int count = 0;
+          int number = data->line_count;
+          do {
+              count++;
+              number /= 10; 
+          } while (number > 0);
+
+          int len = strlen(FOOTER_PRE) + 2*count;
+         
+          char footer_full[len];
+          memset(footer_full, 0, len);
+          snprintf(footer_full, len, "%s%d", FOOTER_PRE, data->line_count);
+          data->footer = strdup(footer_full);
+          data->footer_length = len;
+
+          printf("data->footer:%s\n", data->footer);
+          TSIOBufferWrite(data->output_buffer, data->footer, data->footer_length);
+          data->output_vio = TSVConnWrite(output_conn, contp, data->output_reader, towrite + data->footer_length + data->header_length);
+          TSContCall(TSVIOContGet(write_vio), TS_EVENT_VCONN_WRITE_COMPLETE, write_vio);
+        }
+      }
     }
-     
   }
 }
 
 static int
 add_text_transform(TSCont contp, TSEvent event, void *edata ATS_UNUSED)
 {
-  if (TSVConnClosedGet(contp)) {
+  if (TSVConnClosedGet(contp))
+  {
     my_data_destroy(static_cast<MyData *>(TSContDataGet(contp)));
     TSContDestroy(contp);
     return 0;
-  } else {
-    switch (event) {
-    case TS_EVENT_ERROR: {
+  }
+  else
+  {
+    switch (event)
+    {
+    case TS_EVENT_ERROR:
+    {
       TSVIO write_vio;
       write_vio = TSVConnWriteVIOGet(contp);
       TSContCall(TSVIOContGet(write_vio), TS_EVENT_ERROR, write_vio);
-    } break;
+    }
+    break;
     case TS_EVENT_VCONN_WRITE_COMPLETE:
       TSVConnShutdown(TSTransformOutputVConnGet(contp), 0, 1);
       break;
@@ -213,10 +284,11 @@ add_text_transform(TSCont contp, TSEvent event, void *edata ATS_UNUSED)
 static int
 transformable(TSHttpTxn txnp)
 {
-  int         url_len;
+  int url_len;
   const char *url = TSHttpTxnEffectiveUrlStringGet(txnp, &url_len);
 
-  if (url && (url_len >= 5) && (strncmp(url + url_len - 5, ".m3u8", 5) == 0)) {
+  if (url && (url_len >= 5) && (strncmp(url + url_len - 5, ".m3u8", 5) == 0))
+  {
     return 1;
   }
   return 0;
@@ -234,9 +306,11 @@ transform_plugin(TSCont contp ATS_UNUSED, TSEvent event, void *edata)
 {
   TSHttpTxn txnp = static_cast<TSHttpTxn>(edata);
 
-  switch (event) {
+  switch (event)
+  {
   case TS_EVENT_HTTP_READ_RESPONSE_HDR:
-    if (transformable(txnp)) {
+    if (transformable(txnp))
+    {
       transform_add(txnp);
     }
     TSHttpTxnReenable(txnp, TS_EVENT_HTTP_CONTINUE);
@@ -248,65 +322,26 @@ transform_plugin(TSCont contp ATS_UNUSED, TSEvent event, void *edata)
   return 0;
 }
 
-static int
-load_comment_in_file(const char *filename)
-{
-  FILE *file;
-  long  file_size;
-  // Open the file in read mode
-  file = fopen(filename, "r");
-  if (file == NULL) {
-    TSError("[%s]Error opening file\n[%s]", PLUGIN_NAME, filename);
-    return 0;
-  }
-
-  // Get the size of the file
-  fseek(file, 0, SEEK_END); // Move to the end of the file
-  file_size = ftell(file);  // Get the current position (size of the file)
-  rewind(file);             // Move back to the beginning of the file
-
-  // Allocate memory for the buffer
-  prepend_text = (char *)malloc((file_size + 1) * sizeof(char));
-  if (prepend_text == NULL) {
-    perror("Memory allocation failed");
-    fclose(file);
-    return 0;
-  }
-
-  // Read the file content into the buffer
-  fread(prepend_text, sizeof(char), file_size, file);
-  prepend_text_length = file_size;
-  // Clean up
-  fclose(file);
-
-  return 1;
-}
-
-void
-TSPluginInit(int argc, const char *argv[])
+void TSPluginInit(int argc, const char *argv[])
 {
   TSPluginRegistrationInfo info;
 
-  info.plugin_name   = PLUGIN_NAME;
-  info.vendor_name   = "Your Name";
+  info.plugin_name = PLUGIN_NAME;
+  info.vendor_name = "Your Name";
   info.support_email = "your_email@example.com";
 
-  if (TSPluginRegister(&info) != TS_SUCCESS) {
+  if (TSPluginRegister(&info) != TS_SUCCESS)
+  {
     TSError("[%s] Plugin registration failed", PLUGIN_NAME);
     return;
   }
 
-  if (argc != 2) {
+  if (argc != 2)
+  {
     TSError("[%s] Usage: %s <prepend_text>", PLUGIN_NAME, argv[0]);
     return;
   }
-  int loadRet = load_comment_in_file(argv[1]);
-  if (!loadRet) {
-    TSError("[%s] Failed to load comment in file", PLUGIN_NAME);
-    prepend_text        = strdup(argv[1]);
-    prepend_text_length = strlen(prepend_text);
-  }
+  filepath = strdup(argv[1]);
 
-  TSError("[%s] CONGNV31 Load Comment done:", prepend_text);
   TSHttpHookAdd(TS_HTTP_READ_RESPONSE_HDR_HOOK, TSContCreate(transform_plugin, nullptr));
 }
